b0c1c119d3fa42be3a65b0e287a7defbed60a831
stazynaomivillarta <stazynaomiv@gmail.com>
2026-08-16 16:57:55 +0800

    Activity 2 LE1-Villarta_StazyNaomi.c

---
+++ Activity 2 LE1-Villarta_StazyNaomi.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    printf("----------------------------------------\n");
+    printf("                                      \n");
+    printf("      Welcome to Computer Programming 1\n");
+    printf("                C Programming           \n");
+    printf("                                      \n");
+    printf("|     Learn   Practice   Create        |\n");
+    printf("|                                      |\n");
+    printf("*          Let's Begin!               *\n");
+    printf("*                                      *\n");
+    printf("****************************************\n");
+    return 0;
+}
