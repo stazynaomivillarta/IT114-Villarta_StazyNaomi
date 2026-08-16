8942c1f2f6007e8ed28a8b69359a1bb4d9eb1d13
stazynaomivillarta <stazynaomiv@gmail.com>
2026-08-16 16:46:16 +0800

    LE1-Villarta_StazyNaomi.c

---
+++ LE1-Villarta_StazyNaomi.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    printf("=========================================\n");
+    printf("             STUDENT PROFILE             \n");
+    printf("=========================================\n");
+    printf("Name: Stazy Naomi J. Villarta\n");
+    printf("Course and Section: BSIT-1R14\n");
+    printf("Year Level: 1st Year\n");
+    printf("=========================================\n");
+    printf("            SOFTWARE DEVELOPER           \n");
+    printf("=========================================\n");
+    
+    return 0;
+}
